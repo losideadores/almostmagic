82bd4f3b9c231cece89ad501592a7cb19324de55
Vova <vzakharov@gmail.com>
2023-01-02 22:24:36 +0100

    magically turn objects into guidelines

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ const DEFAULT_URL = 'https://ideality.app/api/polygon'
 
 async function post(baseUrl: string = DEFAULT_URL, url: string, body: object) {
 
+  console.log({baseUrl, url}, body)
+
   const response = await fetch(baseUrl + url, {
     method: 'POST',
     headers: {
@@ -10,10 +12,12 @@ async function post(baseUrl: string = DEFAULT_URL, url: string, body: object) {
     body: JSON.stringify(body)
   })
 
+  // console.log({response})
+
   if (response.ok) {
     return response.json()
   } else {
-    throw new Error(response.statusText)
+    throw new Error(response.toString())
   }
 
 }
@@ -25,6 +29,11 @@ export interface MagicConfig {
   openaiKey?: string
   defaultParameters?: object
   usdSpent?: number
+  keyForGuidelines?: string
+}
+
+interface LooseObject {
+  [key: string]: any
 }
 
 export class Magic {
@@ -63,12 +72,26 @@ export class Magic {
     return data
   }
 
-  async generate(outputKeys: string | string[], input: object, config: MagicConfig = {}) {
+  async generate(outputKeys: string | string[], input: LooseObject, config: MagicConfig = {}) {
 
     const c = Object.assign({}, this.config, config)
 
     if (!Array.isArray(outputKeys)) {
-      outputKeys = [outputKeys]
+
+      // If string, turn into array
+      if (typeof outputKeys === 'string') {
+        outputKeys = [outputKeys]
+      }
+      // If object, turn into array of keys while adding the object under the [keyForGuidelines] key in the input, defaulting to "guidelines"
+      else if (typeof outputKeys === 'object') {
+        const { keyForGuidelines = 'guidelines' } = c
+        // If there is already a key in the input with the same name as the keyForGuidelines, throw an error
+        if ( input[keyForGuidelines] ) {
+          throw new Error(`You cannot use the key "${keyForGuidelines}" in your input object when outputKeys is an object. Please either change the keyForGuidelines config option or change the key in your input object.`)
+        }
+        input[keyForGuidelines] = outputKeys
+        outputKeys = Object.keys(outputKeys)
+      }
     }
 
     const data = await post(c.apiUrl, '/generate', {
@@ -98,7 +121,7 @@ export class Magic {
     return new Magic(config)
   }
 
-  static generate(outputKeys: string | string[], input: object, config: MagicConfig = {}) {
+  static generate(outputKeys: string | string[], input: LooseObject, config: MagicConfig = {}) {
     // (So that you can call Magic.generate() without creating a new instance. We don't do this for run() or upvote() because they are more advanced functions, but with generate we want to give people a way to instantly "feel the magic".)
     // We still need the "config" parameter because they will need to pass in their openaiKey.
     return new Magic(config).generate(outputKeys, input)
